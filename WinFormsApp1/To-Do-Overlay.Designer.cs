9692a7f94cdabd01f4af8a8bf08d97bbe402a703
Tobias Gramlich <119729687+Tobias-Gramlich@users.noreply.github.com>
2024-11-07 09:07:40 +0100

    Add files via upload

---
+++ WinFormsApp1/To-Do-Overlay.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinFormsApp1
+{
+    partial class Overlay
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Overlay));
+            Add = new Button();
+            Eingabename = new TextBox();
+            Liste = new CheckedListBox();
+            Remove = new Button();
+            labelName = new Label();
+            labelDate = new Label();
+            Description = new TextBox();
+            labelDescription = new Label();
+            datum = new DateTimePicker();
+            labelTodo = new Label();
+            RemoveName = new TextBox();
+            label1 = new Label();
+            removeall = new Button();
+            SuspendLayout();
+            // 
+            // Add
+            // 
+            Add.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Add.BackColor = Color.AliceBlue;
+            Add.Location = new Point(659, 21);
+            Add.Name = "Add";
+            Add.Size = new Size(99, 24);
+            Add.TabIndex = 0;
+            Add.Text = "Add";
+            Add.UseVisualStyleBackColor = false;
+            Add.Click += Add_Click;
+            // 
+            // Eingabename
+            // 
+            Eingabename.BackColor = Color.AliceBlue;
+            Eingabename.Location = new Point(12, 23);
+            Eingabename.Name = "Eingabename";
+            Eingabename.ScrollBars = ScrollBars.Horizontal;
+            Eingabename.Size = new Size(151, 23);
+            Eingabename.TabIndex = 1;
+            // 
+            // Liste
+            // 
+            Liste.BackColor = Color.AliceBlue;
+            Liste.FormattingEnabled = true;
+            Liste.Location = new Point(13, 76);
+            Liste.Name = "Liste";
+            Liste.Size = new Size(640, 382);
+            Liste.TabIndex = 2;
+            Liste.ItemCheck += Liste_ItemCheck;
+            // 
+            // Remove
+            // 
+            Remove.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            Remove.BackColor = Color.AliceBlue;
+            Remove.Location = new Point(659, 123);
+            Remove.Name = "Remove";
+            Remove.Size = new Size(99, 24);
+            Remove.TabIndex = 3;
+            Remove.Text = "Remove";
+            Remove.UseVisualStyleBackColor = false;
+            Remove.Click += Remove_Click;
+            // 
+            // labelName
+            // 
+            labelName.AutoSize = true;
+            labelName.Location = new Point(12, 5);
+            labelName.Name = "labelName";
+            labelName.Size = new Size(39, 15);
+            labelName.TabIndex = 5;
+            labelName.Text = "Name";
+            // 
+            // labelDate
+            // 
+            labelDate.AutoSize = true;
+            labelDate.Location = new Point(538, 5);
+            labelDate.Name = "labelDate";
+            labelDate.Size = new Size(31, 15);
+            labelDate.TabIndex = 6;
+            labelDate.Text = "Date";
+            // 
+            // Description
+            // 
+            Description.BackColor = Color.AliceBlue;
+            Description.Location = new Point(169, 23);
+            Description.Name = "Description";
+            Description.Size = new Size(363, 23);
+            Description.TabIndex = 8;
+            // 
+            // labelDescription
+            // 
+            labelDescription.AutoSize = true;
+            labelDescription.Location = new Point(169, 5);
+            labelDescription.Name = "labelDescription";
+            labelDescription.Size = new Size(67, 15);
+            labelDescription.TabIndex = 9;
+            labelDescription.Text = "Description";
+            // 
+            // datum
+            // 
+            datum.CalendarMonthBackground = Color.CornflowerBlue;
+            datum.Format = DateTimePickerFormat.Short;
+            datum.Location = new Point(538, 23);
+            datum.MaxDate = new DateTime(2026, 12, 31, 0, 0, 0, 0);
+            datum.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            datum.Name = "datum";
+            datum.Size = new Size(115, 23);
+            datum.TabIndex = 10;
+            // 
+            // labelTodo
+            // 
+            labelTodo.AutoSize = true;
+            labelTodo.Location = new Point(12, 58);
+            labelTodo.Name = "labelTodo";
+            labelTodo.Size = new Size(47, 15);
+            labelTodo.TabIndex = 11;
+            labelTodo.Text = "To-Do's";
+            // 
+            // RemoveName
+            // 
+            RemoveName.BackColor = Color.AliceBlue;
+            RemoveName.Location = new Point(659, 94);
+            RemoveName.Name = "RemoveName";
+            RemoveName.Size = new Size(201, 23);
+            RemoveName.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(659, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 13;
+            label1.Text = "Remove Name";
+            // 
+            // removeall
+            // 
+            removeall.BackColor = Color.AliceBlue;
+            removeall.Location = new Point(13, 464);
+            removeall.Name = "removeall";
+            removeall.Size = new Size(106, 24);
+            removeall.TabIndex = 19;
+            removeall.Text = "Remove All";
+            removeall.UseVisualStyleBackColor = false;
+            removeall.Click += removeall_Click;
+            // 
+            // Overlay
+            // 
+            AutoScaleMode = AutoScaleMode.Inherit;
+            AutoSize = true;
+            BackColor = Color.Bisque;
+            ClientSize = new Size(979, 552);
+            Controls.Add(removeall);
+            Controls.Add(label1);
+            Controls.Add(RemoveName);
+            Controls.Add(labelTodo);
+            Controls.Add(datum);
+            Controls.Add(labelDescription);
+            Controls.Add(Description);
+            Controls.Add(labelDate);
+            Controls.Add(labelName);
+            Controls.Add(Remove);
+            Controls.Add(Liste);
+            Controls.Add(Eingabename);
+            Controls.Add(Add);
+            ForeColor = SystemColors.ControlText;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Location = new Point(400, 400);
+            Name = "Overlay";
+            Text = "To-Do";
+            TransparencyKey = SystemColors.ControlLight;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Add;
+        private TextBox Eingabename;
+        private CheckedListBox Liste;
+        private Button Remove;
+        private Label labelName;
+        private Label labelDate;
+        private TextBox Description;
+        private Label labelDescription;
+        private DateTimePicker datum;
+        private Label labelTodo;
+        private TextBox RemoveName;
+        private Label label1;
+        private Button removeall;
+    }
+}
---
+++ WinFormsApp1/To-Do-Overlay.cs
@@ -0,0 +1,117 @@
+using System.Data.SQLite;
+
+namespace WinFormsApp1
+{
+    public partial class Overlay : Form
+    {
+
+        public Overlay()
+        {
+            InitializeComponent();
+
+            var conn = new SQLiteConnection("Data Source=C:\\Users\\tgramlich\\Desktop\\S1.P1\\To-Do-List\\Datenbank_todolist.db");
+            conn.Open();
+
+            var createtable = new SQLiteCommand("CREATE TABLE IF NOT EXISTS ToDos (" +
+                                                "Primarykey INTEGER PRIMARY KEY, Name TEXT, Description TEXT, Date TEXT, Done INTEGER)"
+                                                , conn);
+            createtable.ExecuteNonQuery();
+
+            var removetable = new SQLiteCommand("DELETE FROM ToDos WHERE Done IS '1'", conn);
+            removetable.ExecuteNonQuery();
+
+            var Datenlesen = new SQLiteCommand("SELECT * FROM ToDos", conn);
+            var reader = Datenlesen.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string col2 = reader.GetString(1);
+                string col3 = reader.GetString(2);
+                string col4 = reader.GetString(3);
+                int col5 = reader.GetInt32(4);
+
+                if (col5 == 0)
+                {
+                    Liste.Items.Add(col4 + ' ' + col2 + ": " + col3);
+                }
+            }
+            conn.Close();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            Liste.Items.Add(datum.Text + ' ' + Eingabename.Text + ": " + Description.Text);
+
+            var conn = new SQLiteConnection("Data Source=C:\\Users\\tgramlich\\Desktop\\S1.P1\\To-Do-List\\Datenbank_todolist.db");
+            conn.Open();
+
+            var insertdata = new SQLiteCommand("INSERT INTO ToDos (Name, Description, Date, Done) VALUES('"
+                                                + Eingabename.Text.ToString() + "', '"
+                                                + Description.Text.ToString() + "', '"
+                                                + datum.Text.ToString() + "', "
+                                                + "0" + ")", conn);
+            insertdata.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection("Data Source=C:\\Users\\tgramlich\\Desktop\\S1.P1\\To-Do-List\\Datenbank_todolist.db");
+            conn.Open();
+
+            var Datenlesen = new SQLiteCommand("SELECT * FROM ToDos", conn);
+            var reader = Datenlesen.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string col2 = reader.GetString(1);
+
+                if (col2 == RemoveName.Text)
+                {
+                    string col3 = reader.GetString(2);
+                    string col4 = reader.GetString(3);
+
+                    Liste.Items.Remove(col4 + ' ' + col2 + ": " + col3);
+
+                    var removetable = new SQLiteCommand("DELETE FROM ToDos WHERE Name IS '" + col2 + "'", conn);
+                    removetable.ExecuteNonQuery();
+                }
+            }
+            conn.Close();
+        }
+
+        private void removeall_Click(object sender, EventArgs e)
+        {
+            var conn = new SQLiteConnection("Data Source=C:\\Users\\tgramlich\\Desktop\\S1.P1\\To-Do-List\\Datenbank_todolist.db");
+            conn.Open();
+
+            Liste.Items.Clear();
+            var removeall = new SQLiteCommand("DROP TABLE IF EXISTS ToDos", conn);
+            removeall.ExecuteNonQuery();
+
+            conn.Close();
+        }
+
+        private void Liste_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            var conn = new SQLiteConnection("Data Source=C:\\Users\\tgramlich\\Desktop\\S1.P1\\To-Do-List\\Datenbank_todolist.db");
+            conn.Open();
+
+            int index = e.Index + 1;
+
+            if (e.NewValue.ToString() == "Checked")
+            {
+                var checkeddata = new SQLiteCommand("UPDATE ToDos SET Done = 1 WHERE PrimaryKey = '" + index.ToString() + "'", conn);
+                checkeddata.ExecuteNonQuery();
+            }
+            else
+            {
+                var checkeddata = new SQLiteCommand("UPDATE ToDos SET Done = 0 WHERE PrimaryKey = '" + index.ToString() + "'", conn);
+                checkeddata.ExecuteNonQuery();
+            }
+
+            conn.Close();
+        }
+    }
+}
